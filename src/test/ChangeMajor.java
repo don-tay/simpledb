babb64b6e668636ec4fc4efd7bc9daa8001623a6
Don Tay Xi Cheng <dontay0209@gmail.com>
2022-01-25 16:40:53 +0800

    Initial commit

---
+++ src/test/ChangeMajor.java
@@ -0,0 +1,27 @@
+package test;
+
+import simpledb.plan.Planner;
+import simpledb.server.SimpleDB;
+import simpledb.tx.Transaction;
+
+public class ChangeMajor {
+   public static void main(String[] args) {
+      try {
+ 	     // analogous to the driver
+ 	     SimpleDB db = new SimpleDB("studentdb");
+ 	
+ 	     // analogous to the connection
+ 	     Transaction tx  = db.newTx();
+ 	     Planner planner = db.planner();
+         String cmd = "update STUDENT "
+                    + "set MajorId=30 "
+                    + "where SName = 'amy'";
+         planner.executeUpdate(cmd, tx);
+         System.out.println("Amy is now a drama major.");
+         tx.commit();
+      }
+      catch(Exception e) {
+         e.printStackTrace();
+      }
+   }
+}
---
+++ src/test/SimpleIJ.java
@@ -0,0 +1,99 @@
+package test;
+
+import java.sql.Types;
+import java.util.List;
+import java.util.Scanner;
+
+import simpledb.plan.Plan;
+import simpledb.plan.Planner;
+import simpledb.query.Scan;
+import simpledb.server.SimpleDB;
+import simpledb.tx.Transaction;
+
+public class SimpleIJ {
+	private static final String DEFAULT_DB_NAME = "studentdb";
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+//		Default to studentdb for faster testing 
+//		System.out.print("Enter the name of DB to connect: ");
+//		String dbName = (sc.hasNext()) ? sc.next().trim() : DEFAULT_DB_NAME;
+		String dbName = DEFAULT_DB_NAME;
+
+		try {
+			// analogous to the driver
+			SimpleDB db = new SimpleDB(dbName);
+
+			// analogous to the connection
+			Transaction tx = db.newTx();
+			Planner planner = db.planner();
+
+			System.out.println("Successfully connected to " + dbName);
+			System.out.print("\nSQL> ");
+			while (sc.hasNextLine()) {
+				// process one line of input
+				String cmd = sc.nextLine().trim();
+				if (cmd.startsWith("exit"))
+					break;
+				else if (cmd.startsWith("select"))
+					doQuery(planner, cmd, tx);
+				else
+					doUpdate(planner, cmd, tx);
+				System.out.print("\nSQL> ");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		sc.close();
+		System.exit(0);
+	}
+
+	private static void doQuery(Planner planner, String cmd, Transaction tx) {
+		try {
+			Plan p = planner.createQueryPlan(cmd, tx);
+			List<String> fieldNames = p.schema().fields();
+			int totalWidth = 0;
+
+			// print header
+			for (String fieldName : fieldNames) {
+				int width = fieldName.length();
+				totalWidth += width;
+				String fmt = "%" + width + "s";
+				System.out.format(fmt, fieldName);
+			}
+			System.out.println();
+			for (int i = 0; i < totalWidth; i++)
+				System.out.print("-");
+			System.out.println();
+
+			Scan s = p.open();
+			// print records
+			while (s.next()) {
+				for (String fieldName : fieldNames) {
+					int fldtype = p.schema().type(fieldName);
+					String fmt = "%" + fieldName.length();
+					if (fldtype == Types.INTEGER) {
+						int ival = s.getInt(fieldName);
+						System.out.format(fmt + "d", ival);
+					} else {
+						String sval = s.getString(fieldName);
+						System.out.format(fmt + "s", sval);
+					}
+				}
+				System.out.println();
+			}
+		} catch (Exception e) {
+			System.out.println("SQL Exception: " + e.getMessage());
+		}
+	}
+
+	private static void doUpdate(Planner planner, String cmd, Transaction tx) {
+		try {
+			int numRecordsUpdated = planner.executeUpdate(cmd, tx);
+			tx.commit();
+			System.out.println(numRecordsUpdated + " records processed");
+		} catch (Exception e) {
+			System.out.println("Exception: " + e.getMessage());
+		}
+	}
+}
