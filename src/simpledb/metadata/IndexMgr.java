ce892b1109c9b4d5346ddb85f37ef8a7f2b4b95c
Tishya Khanna <tishya.khanna01@gmail.com>
2022-02-05 21:05:11 +0800

    change constructors

--- src/simpledb/metadata/IndexMgr.java
+++ src/simpledb/metadata/IndexMgr.java
@@ -29,6 +29,7 @@ class IndexMgr {
          sch.addStringField("indexname", MAX_NAME);
          sch.addStringField("tablename", MAX_NAME);
          sch.addStringField("fieldname", MAX_NAME);
+         sch.addStringField("indextype", MAX_NAME);
          tblmgr.createTable("idxcat", sch, tx);
       }
       this.tblmgr = tblmgr;
@@ -43,14 +44,17 @@ class IndexMgr {
     * @param idxname the name of the index
     * @param tblname the name of the indexed table
     * @param fldname the name of the indexed field
+    * @param indextype the type of the index
     * @param tx the calling transaction
     */
-   public void createIndex(String idxname, String tblname, String fldname, Transaction tx) {
+   public void createIndex(String idxname, String tblname, String fldname, 
+                           String indextype,Transaction tx) {
       TableScan ts = new TableScan(tx, "idxcat", layout);
       ts.insert();
       ts.setString("indexname", idxname);
       ts.setString("tablename", tblname);
       ts.setString("fieldname", fldname);
+      ts.setString("indextype", indextype);
       ts.close();
    }
    
@@ -68,9 +72,10 @@ class IndexMgr {
          if (ts.getString("tablename").equals(tblname)) {
          String idxname = ts.getString("indexname");
          String fldname = ts.getString("fieldname");
+         String indextype = ts.getString("indextype");
          Layout tblLayout = tblmgr.getLayout(tblname, tx);
          StatInfo tblsi = statmgr.getStatInfo(tblname, tblLayout, tx);
-         IndexInfo ii = new IndexInfo(idxname, fldname, tblLayout.schema(), tx, tblsi);
+         IndexInfo ii = new IndexInfo(idxname, fldname, indextype, tblLayout.schema(), tx, tblsi);
          result.put(fldname, ii);
       }
       ts.close();
--- src/simpledb/parse/CreateIndexData.java
+++ src/simpledb/parse/CreateIndexData.java
@@ -5,15 +5,16 @@ package simpledb.parse;
  * @author Edward Sciore
  */
 public class CreateIndexData {
-   private String idxname, tblname, fldname;
+   private String idxname, tblname, fldname, indextype;
    
    /**
     * Saves the table and field names of the specified index.
     */
-   public CreateIndexData(String idxname, String tblname, String fldname) {
+   public CreateIndexData(String idxname, String tblname, String fldname, String indextype) {
       this.idxname = idxname;
       this.tblname = tblname;
       this.fldname = fldname;
+      this.indextype = indextype;
    }
    
    /**
@@ -39,5 +40,12 @@ public class CreateIndexData {
    public String fieldName() {
       return fldname;
    }
-}
 
+/**
+    * Returns the type of the index.
+    * @return the type of the index
+    */
+    public String indexType() {
+      return indextype;
+   }
+}
