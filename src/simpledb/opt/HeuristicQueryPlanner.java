ed3b1d8d98437faceebb37fc3f1fe40dbf32948a
Don Tay Xi Cheng <dontay0209@gmail.com>
2022-02-13 14:52:55 +0800

    Modify heuristicqueryplanner with optional sort plan

--- src/simpledb/opt/HeuristicQueryPlanner.java
+++ src/simpledb/opt/HeuristicQueryPlanner.java
@@ -2,54 +2,61 @@ package simpledb.opt;
 
 import java.util.*;
 import simpledb.tx.Transaction;
+import simpledb.materialize.SortPlan;
 import simpledb.metadata.MetadataMgr;
 import simpledb.parse.QueryData;
 import simpledb.plan.*;
 
 /**
  * A query planner that optimizes using a heuristic-based algorithm.
+ * 
  * @author Edward Sciore
  */
 public class HeuristicQueryPlanner implements QueryPlanner {
    private Collection<TablePlanner> tableplanners = new ArrayList<>();
    private MetadataMgr mdm;
-   
+
    public HeuristicQueryPlanner(MetadataMgr mdm) {
       this.mdm = mdm;
    }
-   
+
    /**
-    * Creates an optimized left-deep query plan using the following
-    * heuristics.
-    * H1. Choose the smallest table (considering selection predicates)
-    * to be first in the join order.
-    * H2. Add the table to the join order which
-    * results in the smallest output.
+    * Creates an optimized left-deep query plan using the following heuristics. H1.
+    * Choose the smallest table (considering selection predicates) to be first in
+    * the join order. H2. Add the table to the join order which results in the
+    * smallest output.
     */
    public Plan createPlan(QueryData data, Transaction tx) {
-      
-      // Step 1:  Create a TablePlanner object for each mentioned table
+
+      // Step 1: Create a TablePlanner object for each mentioned table
       for (String tblname : data.tables()) {
          TablePlanner tp = new TablePlanner(tblname, data.pred(), tx, mdm);
          tableplanners.add(tp);
       }
-      
-      // Step 2:  Choose the lowest-size plan to begin the join order
+
+      // Step 2: Choose the lowest-size plan to begin the join order
       Plan currentplan = getLowestSelectPlan();
-      
-      // Step 3:  Repeatedly add a plan to the join order
+
+      // Step 3: Repeatedly add a plan to the join order
       while (!tableplanners.isEmpty()) {
          Plan p = getLowestJoinPlan(currentplan);
          if (p != null)
             currentplan = p;
-         else  // no applicable join
+         else // no applicable join
             currentplan = getLowestProductPlan(currentplan);
       }
-      
-      // Step 4.  Project on the field names and return
-      return new ProjectPlan(currentplan, data.fields());
+
+      // Step 4. Project on the field names and return
+      currentplan = new ProjectPlan(currentplan, data.fields());
+
+      // Step 5: Sort on the records
+      if (!data.sortfields().isEmpty()) {
+         currentplan = new SortPlan(tx, currentplan, data.sortfields());
+      }
+
+      return currentplan;
    }
-   
+
    private Plan getLowestSelectPlan() {
       TablePlanner besttp = null;
       Plan bestplan = null;
@@ -63,7 +70,7 @@ public class HeuristicQueryPlanner implements QueryPlanner {
       tableplanners.remove(besttp);
       return bestplan;
    }
-   
+
    private Plan getLowestJoinPlan(Plan current) {
       TablePlanner besttp = null;
       Plan bestplan = null;
@@ -78,7 +85,7 @@ public class HeuristicQueryPlanner implements QueryPlanner {
          tableplanners.remove(besttp);
       return bestplan;
    }
-   
+
    private Plan getLowestProductPlan(Plan current) {
       TablePlanner besttp = null;
       Plan bestplan = null;
