35d9326d38fbafd84901621a99aca73798600221
Tishya Khanna <tishya.khanna01@gmail.com>
2022-03-08 14:50:20 +0800

    revert to previous logic

--- src/simpledb/materialize/MergeJoinScan.java
+++ src/simpledb/materialize/MergeJoinScan.java
@@ -45,7 +45,6 @@ public class MergeJoinScan implements Scan {
     */
    public void beforeFirst() {
       s1.beforeFirst();
-      hasmore1 = s1.next();
       s2.beforeFirst();
    }
    
@@ -63,13 +62,11 @@ public class MergeJoinScan implements Scan {
     * @see simpledb.query.Scan#next()
     */
    public boolean next() {
-      if (!hasmore1) {
-         return false;
-      }
       boolean hasmore2 = s2.next();
       if (hasmore2 && s2.getVal(fldname2).equals(joinval))
          return true;
       
+      boolean hasmore1 = s1.next();
       if (hasmore1 && s1.getVal(fldname1).equals(joinval)) {
          s2.restorePosition();
          return true;
